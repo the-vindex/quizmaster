342a05b5efc2396c43b037bc867b73a31605de2a
Karel Smutný <karel.smutny@outlook.com>
2024-08-24 23:45:39 +0200

    Setup Gradle to build frontend and add it to backend .jar

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.siouan.frontend-jdk21") version "8.1.0"
 }
 
 group = "cz.scrumdojo"
@@ -11,6 +12,14 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
+}
+
+frontend {
+    cacheDirectory.set(project.layout.projectDirectory.dir(".gradle/org.siouan.frontend-jdk21"))
+    nodeVersion.set("22.7.0")
+    nodeInstallDirectory.set(project.layout.projectDirectory.dir("../frontend/node"))
+    packageJsonDirectory.set(project.layout.projectDirectory.dir("../frontend"))
+    assembleScript.set("run build")
 }
 
 configurations {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,10 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
     plugins: [solid()],
+    build: {
+        outDir: '../backend/src/main/resources/static',
+        emptyOutDir: true,
+    },
     server: {
         proxy: {
             '/api': 'http://localhost:8080',
