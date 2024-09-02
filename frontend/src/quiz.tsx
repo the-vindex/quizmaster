cbc096a15a97e536a4a46c0507d6e406f87bb8cf
Karel Smutný <karel.smutny@outlook.com>
2024-09-02 13:18:14 +0200

    <Question/> components works internally with answer idx

--- frontend/src/quiz.tsx
+++ frontend/src/quiz.tsx
@@ -1,4 +1,4 @@
-import { createSignal, For, onMount, Show } from 'solid-js'
+import { type Accessor, createSignal, For, onMount, Show } from 'solid-js'
 import { useParams } from '@solidjs/router'
 import type { QuizQuestion } from 'model/quiz-question.ts'
 import { preventDefault } from 'helpers.ts'
@@ -6,17 +6,17 @@ import { preventDefault } from 'helpers.ts'
 const Feedback = (correct: boolean) => <p class="feedback">{correct ? 'Correct!' : 'Incorrect!'}</p>
 
 const Question = ({ question, answers }: QuizQuestion) => {
-    const [selectedAnswer, setSelectedAnswer] = createSignal<string | null>(null)
-    const isCorrect = () => selectedAnswer() === 'Paris'
+    const [selectedAnswer, setSelectedAnswer] = createSignal<number | null>(null)
+    const isCorrect = () => selectedAnswer() === 2
 
     const [submitted, setSubmitted] = createSignal(false)
     const submit = preventDefault(() => setSubmitted(true))
 
-    const selectAnswer = (answer: string) => () => setSelectedAnswer(answer)
+    const selectAnswer = (answerIdx: number) => () => setSelectedAnswer(answerIdx)
 
-    const Answer = (answer: string) => (
+    const Answer = (answer: string, idx: Accessor<number>) => (
         <li>
-            <input type={'radio'} name={'answer'} value={answer} onClick={selectAnswer(answer)} />
+            <input type={'radio'} name={'answer'} value={answer} onClick={selectAnswer(idx())} />
             {answer}
         </li>
     )
