5c5cf8123c3b2660266a1495866268ce52a0be70
Karel Smutný <karel.smutny@outlook.com>
2024-09-02 10:39:01 +0200

    Refactored feedback component

--- frontend/src/quiz.tsx
+++ frontend/src/quiz.tsx
@@ -1,10 +1,14 @@
-import { createSignal, For, Match, onMount, Show, Switch } from 'solid-js'
+import { createSignal, For, onMount, Show } from 'solid-js'
 import { useParams } from '@solidjs/router'
 import type { QuizQuestion } from 'model/quiz-question.ts'
 import { preventDefault } from 'helpers.ts'
 
+const Feedback = (correct: boolean) => <p class="feedback">{correct ? 'Correct!' : 'Incorrect!'}</p>
+
 const Question = ({ question, answers }: QuizQuestion) => {
     const [selectedAnswer, setSelectedAnswer] = createSignal<string | null>(null)
+    const isCorrect = () => selectedAnswer() === 'Paris'
+
     const [submitted, setSubmitted] = createSignal(false)
     const submit = preventDefault(() => setSubmitted(true))
 
@@ -24,16 +28,7 @@ const Question = ({ question, answers }: QuizQuestion) => {
                 <For each={answers} children={Answer} />
             </ul>
             <input type="submit" value={'Submit'} />
-            <Show when={submitted()}>
-                <Switch>
-                    <Match when={selectedAnswer() === 'Paris'}>
-                        <p class="feedback">Correct!</p>
-                    </Match>
-                    <Match when={selectedAnswer() !== 'Paris'}>
-                        <p class="feedback">Incorrect!</p>
-                    </Match>
-                </Switch>
-            </Show>
+            <Show when={submitted()} children={Feedback(isCorrect())} keyed />
         </form>
     )
 }
