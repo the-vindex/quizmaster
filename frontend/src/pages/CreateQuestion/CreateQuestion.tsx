7d78eafb627c9af89fdc0e6de5962a4c4df8ac70
jan.dudziak <jan.dudziak@4finance.com>
2024-09-05 13:28:56 +0200

    Create question form style fix

--- frontend/src/pages/CreateQuestion/CreateQuestion.tsx
+++ frontend/src/pages/CreateQuestion/CreateQuestion.tsx
@@ -1,49 +1,43 @@
 import { createSignal } from 'solid-js';
-import './createQuestion.css'; // Import the CSS module
+import './createQuestion.css';
 
 function QuizForm() {
     const [question, setQuestion] = createSignal<string>('');
-    // uncomment to include answers
-    // const [answers, setAnswers] = createSignal<string[]>(['', '', '', '']);
-    // const [correctAnswer, setCorrectAnswer] = createSignal<number | null>(null);
-    // uncomment to include explanations
-    // const [questionExplanations, setQuestionExplanations] = createSignal<string[]>(['', '', '', '']);
-    // const [answerExplanation, setAnswerExplanations] = createSignal<string>('');
-    {/*
-  const updateAnswer = (index: number, value: string) => {
+    const [answers, setAnswers] = createSignal<string[]>(['', '', '', '']);
+    const [correctAnswer, setCorrectAnswer] = createSignal<number | null>(null);
+    const [questionExplanations, setQuestionExplanations] = createSignal<string[]>(['', '', '', '']);
+    const [answerExplanation, setAnswerExplanation] = createSignal<string>('');
 
-    //setTimeout(() => console.log("debouncing 500 ms"), 500)
-    setAnswers(prev => {
-      const newAnswers = [...prev];
-      newAnswers[index] = value;
-      return newAnswers;
-    });
-  };*/}
-    // uncomment to include explanation
-    /*
-      const updateExplanation = (index: number, value: string) => {
-        setQuestionExplanations(prev => {
-          const newExplanations = [...prev];
-          newExplanations[index] = value;
-          return newExplanations;
+    const updateAnswer = (index: number, value: string) => {
+
+        setAnswers(prev => {
+            const newAnswers = [...prev];
+            newAnswers[index] = value;
+            return newAnswers;
         });
-      }; */
+    };
+    const updateExplanation = (index: number, value: string) => {
+        setQuestionExplanations(prev => {
+            const newExplanations = [...prev];
+            newExplanations[index] = value;
+            return newExplanations;
+        });
+    };
 
     const handleSubmit = (e: Event) => {
         e.preventDefault();
         const formData = {
             question: question(),
-            // answers: answers(),
-            // correctAnswer: correctAnswer(),
-            // uncomment to include Explanations
-            // questionExplanations: questionExplanations(),
-            // generalExplanations: generalExplanations(),
+            answers: answers(),
+            correctAnswer: correctAnswer(),
+            questionExplanations: questionExplanations(),
+            generalExplanations: answerExplanation(),
         };
         console.log(formData); // Handle form data submission
     };
 
 
-    {// const answersArray = answers()
+    { const answersArray = answers()
         return (
             <form class="question-create-form" onSubmit={handleSubmit}>
                 {/* Question input */}
@@ -59,43 +53,42 @@ function QuizForm() {
                 </div>
 
                 {/* Answer rows */}
-                {/*answersArray.map((_answer, index) => (
-        <div class="answerRow">
-          <input
-            type="text"
-            placeholder={`Answer ${index +1}`}
-            value={answers()[index]}
-            onInput={(e) => updateAnswer(index, (e.target as HTMLInputElement).value)}
-            class="answerInput"
-          />
-          <input
-            type="checkbox"
-            checked={correctAnswer() === index}
-            onChange={() => setCorrectAnswer(index)}
-            class="checkbox"
-          />{// uncomment to include Explanations
-            /*
-          <input
-            type="text"
-            placeholder="Explanations for wrong answer"
-            value={questionExplanations()[index]}
-            onInput={(e) => updateExplanation(index, (e.target as HTMLInputElement).value)}
-            class="explanationInput"
-          />}
-        </div>
-      ))}
+                {answersArray.map((_answer, index) => (
+                    <div class="answerRow">
+                        <input
+                            type="text"
+                            placeholder={`Answer ${index +1}`}
+                            value={answers()[index]}
+                            onInput={(e) => updateAnswer(index, (e.target as HTMLInputElement).value)}
+                            class="answerInput"
+                        />
+                        <input
+                            type="checkbox"
+                            checked={correctAnswer() === index}
+                            onChange={() => setCorrectAnswer(index)}
+                            class="checkbox"
+                        />{
+                        <input
+                            type="text"
+                            placeholder="Explanations for wrong answer"
+                            value={questionExplanations()[index]}
+                            onInput={(e) => updateExplanation(index, (e.target as HTMLInputElement).value)}
+                            class="explanationInput"
+                        />}
+                    </div>
+                ))}
 
-      {// uncomment to include explanation
-      /*
-      <div>
-        <label>General explanation for the entire question:</label>
-        <textarea
-          class="generalExplanation"
-          value={generalExplanation()}
-          onInput={(e) => setGeneralExplanation((e.target as HTMLTextAreaElement).value)}
-          rows="2"
-        />
-      </div>*/}
+                {
+
+                    <div>
+                        <label>General explanation for the entire question:</label>
+                        <textarea
+                            class="generalExplanation"
+                            value={answerExplanation()}
+                            onInput={(e) => setAnswerExplanation((e.target as HTMLTextAreaElement).value)}
+                            rows="2"
+                        />
+                    </div>}
 
                 {/* Submit button */}
                 <button type="submit" class="submitButton">Submit</button>
