bf93c0311e439babf3e823eab4157e070ec4e00e
Vaclav Jirak <jirakv95@gmail.com>
2024-12-05 05:39:47 +0100

    Add QuizAnswer entity

---
+++ backend/src/main/java/cz/scrumdojo/quizmaster/quiz/QuizAnswer.java
@@ -0,0 +1,30 @@
+package cz.scrumdojo.quizmaster.quiz;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class QuizAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "quiz_run_id", nullable = false)
+    private Integer quizRunId;
+
+    @Column(name = "question_id", nullable = false)
+    private Integer questionId;
+
+    @Column(name = "answers", columnDefinition = "text[]")
+    @JdbcTypeCode(SqlTypes.ARRAY)
+    private int[] answers;
+
+}
