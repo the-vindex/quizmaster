15cc6f4e1986ef2c7048ee52fb42c495b6c12b3b
Miloslav Kral <milkral@csas.cz>
2024-12-05 13:46:26 +0100

    fix: formatting

--- frontend/src/pages/QuizResult.tsx
+++ frontend/src/pages/QuizResult.tsx
@@ -1,5 +1,3 @@
 export function QuizResult() {
-    return (
-            <h1 id="quiz-result-page">Quiz Result Page</h1>
-            )
+    return <h1 id="quiz-result-page">Quiz Result Page</h1>
 }
