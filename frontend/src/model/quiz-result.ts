159e2f7bcd3677edaf939ef7e3ace3d917dd2a50
Marek Polacik <mpolacik@csas.cz>
2024-12-05 16:07:35 +0100

    feat(result): quiz result FE models

---
+++ frontend/src/model/quiz-result.ts
@@ -0,0 +1,14 @@
+import {Quiz, QuizQuestion} from "./quiz-question.ts";
+
+export interface QuizQuestionResult {
+    readonly selectedAnswers: number[]
+    readonly explanationToShowIds: number[] // indexy do question.explanations[]
+    readonly question: QuizQuestion
+}
+
+export type QuizRunResult = {
+    readonly runId: number
+    readonly quizId: number
+    readonly quiz: Quiz
+    readonly questionResults: QuizQuestionResult[]
+}
